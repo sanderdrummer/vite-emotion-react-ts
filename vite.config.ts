62a8322208208ed95d2a8020f9292cf818e2e4ee
sanderdrummer <tobias.pickel@moia.io>
2022-12-14 08:26:14 +0100

    feat: swc setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,9 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "plugin-react-swc-emotion";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    jsxFactory: "jsx",
-    jsxInject: `import { jsx } from '@emotion/react'`,
-  },
-  test: {
-    global: true,
-    environment: "jsdom",
-    setupFiles: ["./src/setupTests.ts"],
-  },
-  plugins: [
-    react({
-      jsxImportSource: "@emotion/react",
-      babel: {
-        plugins: ["@emotion/babel-plugin"],
-      },
-    }),
-  ],
+  plugins: [react()],
 });
