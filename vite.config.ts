5d7502bb2a461fe36d062348c1fcf2f0c9831e65
sanderdrummer <tobias.pickel@moia.io>
2023-02-28 10:49:02 +0100

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from "vite";
-import react from "plugin-react-swc-emotion";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+    }),
+  ],
 });
